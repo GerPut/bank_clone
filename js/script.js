f7bacc38170d106685c33f2f1ab826e36387ca21
Gerhard <gerhard.putter@yahoo.com>
2021-05-05 09:54:04 +0200

    Worked on navbar for smaller screens not complete.

--- js/script.js
+++ js/script.js
@@ -34,6 +34,20 @@ function showSlides() {
 
 
 
+//NAVBAR SMALL SCREENS ///////////////////////////////////////////////////////////
+
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
+
+
+
 
 
 
